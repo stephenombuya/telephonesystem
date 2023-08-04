35d54c789d4c7d1eb63a5b6674acb771a339cd5e
Stephen ombuya <129269603+STEPHEN07383@users.noreply.github.com>
2023-08-04 15:24:22 +0300

    Add files via upload

---
+++ DeskPhone.java
@@ -0,0 +1,63 @@
+package telephonesystem;
+
+public class DeskPhone implements Telephone {
+
+			private String myNumber;
+			private boolean isRinging;
+			private boolean isPowerOn;
+			
+
+			public DeskPhone(String myNumber) {
+				this.myNumber=myNumber;
+			}
+
+
+			@Override
+			public void powerOn() {
+				isPowerOn = true;
+				System.out.println("Phone powered on");
+				
+			}
+
+
+			@Override
+			public void dial(String phoneNumber) {
+				System.out.println("Now ringing " + phoneNumber + " on deskphone ");
+				
+			}
+
+
+			@Override
+			public void answer() {
+				if(isRinging & isPowerOn) {
+					System.out.println("Answering the desk phone");
+					isRinging = false;
+				}
+				else {
+					
+				}
+				
+			}
+
+
+			@Override
+			public boolean callPhone(String phoneNumber) {
+				if(phoneNumber == myNumber & isPowerOn) {
+					isRinging = true;
+					System.out.println("Phone ringing");
+				}
+				else {
+					System.out.println("Either wrong number entered or phone is off");
+					isRinging = false;
+				}
+				return isRinging;
+			}
+
+
+			@Override
+			public boolean isRinging() {
+				return isRinging;
+			}
+
+		}
+
