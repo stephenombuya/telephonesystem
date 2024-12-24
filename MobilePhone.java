746193b1951059a936cd31fe37e215a4692240d7
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2024-12-24 12:44:05 +0300

    Update MobilePhone.java

--- MobilePhone.java
+++ MobilePhone.java
@@ -1,66 +1,13 @@
 package telephonesystem;
 
-public class MobilePhone implements Telephone{
-		private String myNumber;
-		private boolean isRinging;
-		private boolean isPowerOn;
-		
+public class MobilePhone extends AbstractPhone {
 
-		public MobilePhone(String myNumber) {
-			this.myNumber=myNumber;
-		}
+    public MobilePhone(String myNumber) {
+        super(myNumber);
+    }
 
-
-		@Override
-		public void powerOn() {
-			isPowerOn = true;
-			System.out.println("Phone powered on");
-			
-		}
-
-
-		@Override
-		public void dial(String phoneNumber) {
-			System.out.println("Now ringing " + phoneNumber + " on deskphone ");
-			
-		}
-
-
-		@Override
-		public void answer() {
-			if(isRinging & isPowerOn) {
-				System.out.println("Answering the mobile phone");
-				isRinging = false;
-			}
-			else {
-				
-			}
-			
-		}
-
-
-		@Override
-		public boolean callPhone(String phoneNumber) {
-			if(phoneNumber == myNumber & isPowerOn) {
-				isRinging = true;
-				System.out.println("Phone ringing");
-			}
-			else {
-				System.out.println("Either wrong number entered or phone is off");
-				isRinging = false;
-			}
-			return isRinging;
-		}
-
-
-		@Override
-		public boolean isRinging() {
-			return isRinging;
-		}
-		
-		
-	}
-
-
-
-
+    @Override
+    public void dial(String phoneNumber) {
+        System.out.println("Now ringing " + phoneNumber + " on mobile phone");
+    }
+}
